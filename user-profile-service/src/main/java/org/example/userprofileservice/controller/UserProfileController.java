2abbbffbfa7c3c7b501b71acb296ec847a770438
Aidyn <nurlan.aydin06@gmail.com>
2024-10-11 14:37:20 +0500

    UserProfiler entity added

--- user-profile-service/src/main/java/org/example/userprofileservice/controller/UserProfileController.java
+++ user-profile-service/src/main/java/org/example/userprofileservice/controller/UserProfileController.java
@@ -1,8 +1,13 @@
 package org.example.userprofileservice.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/user-profile")
 public class UserProfileController {
+
+
+
 }
--- user-profile-service/src/main/java/org/example/userprofileservice/entity/UserProfile.java
+++ user-profile-service/src/main/java/org/example/userprofileservice/entity/UserProfile.java
@@ -1,4 +1,27 @@
 package org.example.userprofileservice.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "profiles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(name = "user_id")
+    Long userId;
+    String firstname;
+    String lastname;
+    @Column(name = "phone_number")
+    String phoneNumber;
 }
